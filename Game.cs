3af4dde70d1df9d4dacede09edcc4a8be26583c9
Shiny Gyrodos <gibbs2kids@gmail.com>
2024-07-09 21:47:27 -0500

    Added new file (obviously), class, and struct. Added GetPlayerChoice, Hit, and Call methods.

--- Game.cs
+++ Game.cs
@@ -1,13 +1,61 @@
+using System.Reflection.Metadata;
+struct Hand()
+{
+    private int index = 0;
+    private int[] cards = new int[5];
+    public int Total => cards.Sum();
+    public void AddCard(int value) => cards[index++] = value;
+}
+
+
+
 class Game
 {
-    static void GetPlayerChoice()
-    {
-        string playerChoice = "to be filled";
+    private const int blackjack = 21;
 
-        do
+
+    static string GetPlayerChoice()
+    {
+        string playerChoice = "";
+
+        while (playerChoice != "hit" && playerChoice != "call")
         {
             playerChoice = Console.ReadLine().ToLower() ?? "null reference";
-            
-        } while (playerChoice != "hit" && playerChoice != "call");
+        }
+
+        return playerChoice;
+    }
+
+
+
+    static void Call(Hand playerHand, Deck deck)
+    {
+        Console.WriteLine("The dealer will now draw cards.");
+        Hand dealerHand = new();
+        string message = "YOU LOSE. Dealer is closer.";
+
+        while (dealerHand.Total < playerHand.Total)
+        {
+            try
+            {
+                Console.Write(Hit(dealerHand, deck) + " ");
+            }
+            catch (IndexOutOfRangeException)
+            {
+                message = "YOU WIN! Dealer busts.";
+                break;
+            }
+        }
+
+        Console.WriteLine(message);
+    }
+
+
+
+    static int Hit(Hand hand, Deck deck)
+    {
+        int cardDrawn = deck.Draw();
+        hand.AddCard(cardDrawn == 11 && hand.Total + 11 > blackjack ? cardDrawn - 10 : cardDrawn);
+        return cardDrawn;
     }
 }
