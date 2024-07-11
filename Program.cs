aafa5ce9f3887d9686b207e6c76158f5977eb8ce
Shiny Gyrodos <gibbs2kids@gmail.com>
2024-07-10 22:34:17 -0500

    Removed some redundant class references

--- Program.cs
+++ Program.cs
@@ -7,12 +7,12 @@ namespace MyApp
         static void Main()
         {
             Deck deck = new();
-            Console.WriteLine(deck.Draw());
-            Console.ReadKey();
+            WriteLine(deck.Draw());
+            ReadKey();
             
-            Console.WriteLine("Ready to play some Blackjack?\n\nPress any key to continue.");
-            Console.Clear();
-            Console.ReadKey();
+            WriteLine("Ready to play some Blackjack?\n\nPress any key to continue.");
+            Clear();
+            ReadKey();
         }
     }
 }
