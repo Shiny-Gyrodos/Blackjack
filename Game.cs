e6cc22c2d827a4c25d6f88f08ce6425d6968ae15
Shiny Gyrodos <gibbs2kids@gmail.com>
2024-07-11 23:05:51 -0500

    General maintenance and bug fixing. At least one bug remains currently.

--- Game.cs
+++ Game.cs
@@ -1,4 +1,5 @@
 using System.Reflection.Metadata;
+using System.Security.Cryptography;
 struct Hand()
 {
     private int index = 0;
@@ -14,7 +15,35 @@ class Game
     private const int blackjack = 21;
 
 
-    static string GetPlayerChoice()
+
+    public static void Start(Deck deck)
+    {
+        Hand playerHand = new();
+        Hand dealerHand = new();
+
+        while (playerHand.cards[4] == 0) // Needs new condition
+        {
+            Display.Refresh(ref playerHand, ref dealerHand);
+
+            switch (GetPlayerChoice())
+            {
+                case "hit":
+                    Hit(ref playerHand, deck);
+                    if (playerHand.cards[4] != 0 && playerHand.Total < 21) Call(ref playerHand, ref dealerHand, deck);
+                    break;
+                case "call":
+                    Call(ref playerHand, ref dealerHand, deck);
+                    break;
+            }
+
+            if (playerHand.Total >= blackjack) WriteLine("YOU LOSE. You went over 21!");
+            else if (playerHand.Total == blackjack) WriteLine("YOU WIN! You got to 21 exactly!");
+        }
+    }
+
+
+
+    private static string GetPlayerChoice()
     {
         string playerChoice = "";
 
@@ -28,31 +57,37 @@ class Game
 
 
 
-    static void Call(Hand playerHand, Deck deck)
+    private static void Call(ref Hand playerHand, ref Hand dealerHand, Deck deck)
     {
-        WriteLine("The dealer will now draw cards.");
-        Hand dealerHand = new();
+        WriteLine("\nThe dealer will now draw cards.");
+        Sleep(3000);
+        Display.Refresh(ref playerHand, ref dealerHand);
         string message = "YOU LOSE. Dealer is closer.";
 
         while (dealerHand.Total < playerHand.Total)
         {
             try
             {
-                Write(Hit(dealerHand, deck) + " ");
+                Hit(ref dealerHand, deck, true);
             }
             catch (IndexOutOfRangeException)
             {
-                message = "YOU WIN! Dealer busts.";
+                message = "YOU WIN! Dealer drew five cards and is lower.";
                 break;
             }
+
+            Display.Refresh(ref playerHand, ref dealerHand);
+            Sleep(1000);
         }
 
-        WriteLine(message);
+        message = dealerHand.Total > blackjack ? "YOU WIN! Dealer busts." : message;
+        WriteLine("\n" + message);
     }
 
 
 
-    static int Hit(Hand hand, Deck deck)
+    // Returns the card drawn for display.
+    private static int Hit(ref Hand hand, Deck deck, bool gameCalled = false)
     {
         int cardDrawn = deck.Draw();
         hand.AddCard(cardDrawn == 11 && hand.Total + 11 > blackjack ? cardDrawn - 10 : cardDrawn);
