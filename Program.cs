11057cd21e7fcc2df8a3d1950a3fd0586c10f246
Shiny Gyrodos <gibbs2kids@gmail.com>
2024-07-11 23:04:50 -0500

    Added some temporary testing code as well as addedthe code for actually playin gthe game.

--- Program.cs
+++ Program.cs
@@ -6,13 +6,34 @@ namespace MyApp
     {
         static void Main()
         {
+            Hand playerHand = new();
+            playerHand.AddCard(1);
+            playerHand.AddCard(2);
+            playerHand.AddCard(3);
+            playerHand.AddCard(4);
+            playerHand.AddCard(5);
+            foreach (int card in playerHand.cards)
+            {
+                Write(card + " ");
+            }
+            Write(playerHand.Total);
+            ReadKey();
+
             Deck deck = new();
-            WriteLine(deck.Draw());
-            ReadKey();
-            
-            WriteLine("Ready to play some Blackjack?\n\nPress any key to continue.");
+
             Clear();
+            WriteLine("Ready to play some Blackjack?\n\nPress any key to continue.");
             ReadKey();
+            Clear();
+            Game.Start(deck);
+            Sleep(5000);
+            Clear();
+            WriteLine("Care to play again? YES - NO");
+
+            if (ReadLine().ToLower() == "yes")
+            {
+                Main();
+            }
         }
     }
 }
