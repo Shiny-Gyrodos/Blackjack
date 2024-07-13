c21759e3dedd7f769935fcd4f752ac3c7a8fc343
Shiny Gyrodos <gibbs2kids@gmail.com>
2024-07-12 23:50:55 -0500

    Fixed an inconsistency in the code.

--- Display.cs
+++ Display.cs
@@ -8,7 +8,8 @@ static class Display
         WriteLine(new string('-', 75));
         WriteLine($"Your total: {playerHand.Total} | Your cards: {GetCard(playerHand, 0)}     {GetCard(playerHand, 1)}     {GetCard(playerHand, 2)}     {GetCard(playerHand, 3)}     {GetCard(playerHand, 4)}");
         WriteLine(new string('-', 75));
-        WriteLine($"Dealer total: {dealerHand.Total} | Dealer cards: {GetCard(dealerHand, 0)}     {GetCard(dealerHand, 1)}     {GetCard(dealerHand, 2)}     {GetCard(dealerHand, 3)}     {GetCard(dealerHand, 4)}\n{new string('-', 75)}");
+        WriteLine($"Dealer total: {dealerHand.Total} | Dealer cards: {GetCard(dealerHand, 0)}     {GetCard(dealerHand, 1)}     {GetCard(dealerHand, 2)}     {GetCard(dealerHand, 3)}     {GetCard(dealerHand, 4)}");
+        WriteLine(new string('-', 75));
 
         static string GetCard(Hand hand, int index) => hand.cards[index] == 0 ? " " : hand.cards[index].ToString();
     }
